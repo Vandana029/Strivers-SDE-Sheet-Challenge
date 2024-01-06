0791eba32ef6b96c6b90a5f36a00bc4ddbcdb3d9
Vandana029 <vandanamaurya0729@gmail.com>
2024-01-06 07:22:02 +0530

    skeleton structure of day 1 Array complete

---
+++ day1 Array/Set Matrix Zero/Better Approach/better.py
@@ -0,0 +1,44 @@
+
+'''
+Time Complexity: O(2*(N*M)), where N = no. of rows in the matrix and M = no. of columns in the matrix.
+Reason: We are traversing the entire matrix 2 times and each traversal is taking O(N*M) time complexity.
+
+Space Complexity: O(N) + O(M), where N = no. of rows in the matrix and M = no. of columns in the matrix.
+Reason: O(N) is for using the row array and O(M) is for using the col array.
+
+'''
+def zeroMatrix(matrix, n, m):
+    row = [0] * n  # row array
+    col = [0] * m  # col array
+
+    # Traverse the matrix:
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j] == 0:
+                # mark ith index of row wih 1:
+                row[i] = 1
+
+                # mark jth index of col wih 1:
+                col[j] = 1
+
+    # Finally, mark all (i, j) as 0
+    # if row[i] or col[j] is marked with 1.
+    for i in range(n):
+        for j in range(m):
+            if row[i] or col[j]:
+                matrix[i][j] = 0
+
+    return matrix
+
+
+if __name__ == "__main__":
+	matrix = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+	n = len(matrix)
+	m = len(matrix[0])
+	ans = zeroMatrix(matrix, n, m)
+
+	print("The Final matrix is:")
+	for row in ans:
+	    for ele in row:
+	        print(ele, end=" ")
+	    print()
